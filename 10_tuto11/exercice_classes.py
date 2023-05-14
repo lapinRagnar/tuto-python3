7b3f1d3b8fb3358d3befb2f8b7c39fa102f21121
lapinRagnar <lapinragnar@gmail.com>
2023-05-14 11:57:56 +0200

    10 class - exemple 1

---
+++ 10_tuto11/exercice_classes.py
@@ -0,0 +1,11 @@
+class Voiture:
+  pass
+
+voiture_bleu = Voiture()
+voiture_rouge = Voiture()
+
+voiture_bleu.couleur = "bleu"
+voiture_rouge.couleur = "rouge"
+
+print(voiture_bleu.couleur)
+print(voiture_rouge.couleur)
