92b87f8ff9b52a89cd61be726dd7896b7dda5546
lapinRagnar <lapinragnar@gmail.com>
2023-05-14 10:47:15 +0200

    2eme maniere de faire des import avec des packages

--- 08_tuto9/exercice1.py
+++ 08_tuto9/exercice1.py
@@ -1,5 +1,6 @@
 from bonjour import dire_bonjour, dire_au_revoir
-import physique
+from physique import trajet
+from physique.vitesse import convertir_ms_en_kmh
 
 nom = input("quel est ton nom?")
 distance = float(input("quel distance veut-tu parcourir?"))
@@ -9,10 +10,10 @@ vitesse_en_ms = float(input("A quelle vitesse? [m/s] "))
 dire_bonjour(nom)
 
 # convertir vitesse en km/h
-vitesse_en_kmh = physique.vitesse.convertir_ms_en_kmh(vitesse_en_ms)
+vitesse_en_kmh = convertir_ms_en_kmh(vitesse_en_ms)
 
 # calculer le temps de trajet
-temps_de_trajet = physique.trajet.temps_de_trajet(distance_en_km=distance, vitesse_en_km_h=vitesse_en_kmh)
+temps_de_trajet = trajet.trajet.temps_de_trajet(distance_en_km=distance, vitesse_en_km_h=vitesse_en_kmh)
 
 # afficher le temps de tranjet
 print(f"votre temps de trajet est de {temps_de_trajet} heures")
