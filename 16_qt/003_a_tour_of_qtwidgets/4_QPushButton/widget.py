fa10a7c5a4de2f5e26939a8ee0adedc41c483f57
lapinRagnar <lapinragnar@gmail.com>
2023-06-14 16:00:48 +0200

    suite QPushButton

--- 16_qt/003_a_tour_of_qtwidgets/4_QPushButton/widget.py
+++ 16_qt/003_a_tour_of_qtwidgets/4_QPushButton/widget.py
@@ -1,5 +1,27 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QVBoxLayout, QPushButton, QWidget
 
 class Widget(QWidget):
   def __init__(self):
     super().__init__()
+    
+    self.setWindowTitle('QPushButton')
+    
+    button = QPushButton('click me')
+    button.clicked.connect(self.button_clicked)
+    button.pressed.connect(self.button_pressed)
+    button.released.connect(self.button_released)
+    
+    layout = QVBoxLayout()
+    layout.addWidget(button)
+    
+    self.setLayout(layout)
+
+  def button_clicked(self):
+    print('button clicked')
+    
+  def button_pressed(self):
+    print('button pressed')
+    
+  def button_released(self):
+    print('button released')
+    
